69022ba1434acb1f9213d61414ef7ff344312917
RowdeyGoos <rowdey_goos@hotmail.com>
2020-06-03 17:27:08 +0200

    Bugfix in apiconnector

--- src/main/java/Client/lib/ClientLibMain.java
+++ src/main/java/Client/lib/ClientLibMain.java
@@ -1,15 +1,9 @@
 package Client.lib;
 
+//Main voor het testen van apiConnector methods
 public class ClientLibMain {
-    /**
-     * This class is only used for directly running methods in clientlib.
-     * So the code here is not used in the rest of the project.
-     *
-     * @param args Command line arguments.
-     */
     public static void main(String[] args) {
         try {
-            //mock-up for demo 2 actions from GUI
 
             //start of application
             ApiConnector apiconnector = new ApiConnector(
